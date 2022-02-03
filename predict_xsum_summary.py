1fe66e7cbee37c218f177e86263b688aa896363c
Daniel Levenson <dleve123@gmail.com>
2022-02-03 15:25:58 -0500

    format and lint

--- predict_xsum_summary.py
+++ predict_xsum_summary.py
@@ -3,10 +3,7 @@ import torch
 import datasets
 
 
-from transformers import (
-    AutoTokenizer,
-    AutoModelForSeq2SeqLM
-)
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -14,23 +11,23 @@ if __name__ == "__main__":
     )
 
     parser.add_argument(
-        '--data_index',
+        "--data_index",
         type=int,
         required=True,
-        help='id of the example in the xsum dataset'
+        help="id of the example in the xsum dataset",
     )
 
     parser.add_argument(
-        '--data_split',
+        "--data_split",
         type=str,
         required=True,
-        choices=['train', 'test', 'validation'],
-        help='xsum data split to index into with `data_index`'
+        choices=["train", "test", "validation"],
+        help="xsum data split to index into with `data_index`",
     )
 
     args = parser.parse_args()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # configure model and tokenizer
     tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-xsum")
@@ -44,17 +41,27 @@ if __name__ == "__main__":
     xsum_data = datasets.load_dataset("xsum")
     xsum_example = xsum_data[args.data_split][args.data_index]
 
-    inputs = tokenizer([xsum_example['document']], max_length=max_num_input_tokens, truncation=True, return_tensors='pt')
+    inputs = tokenizer(
+        [xsum_example["document"]],
+        max_length=max_num_input_tokens,
+        truncation=True,
+        return_tensors="pt",
+    )
     input_token_ids = inputs.input_ids.to(device)
 
     summary_ids = model.generate(
         input_token_ids,
         num_beams=4,
         max_length=max_num_output_tokens,
-        early_stopping=True
+        early_stopping=True,
     )
-    predicted_summary = [tokenizer.decode(id, skip_special_tokens=True, clean_up_tokenization_spaces=False) for id in summary_ids]
-    
-    print("GOLD STANDARD SUMMARY:", xsum_example['summary'])
-    print("PREDICTED SUMMARY:", predicted_summary[0])
 
+    predicted_summary = [
+        tokenizer.decode(
+            id, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )
+        for id in summary_ids
+    ]
+
+    print("GOLD STANDARD SUMMARY:", xsum_example["summary"])
+    print("PREDICTED SUMMARY:", predicted_summary[0])
