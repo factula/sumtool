00466a15e3f724a12b097e919a6ee287ff3bc06d
celine-lee <celine.y.lee@gmail.com>
2022-02-05 11:50:35 -0500

    fix data alignment bugs

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset, DataLoader
-import csv
+import pprint
 
 
 class XsumDataset(Dataset):
@@ -9,7 +9,7 @@ class XsumDataset(Dataset):
         self.factuality_data = factuality_data
         self.faithfulness_data = faithfulness_data
 
-        self.dataset = list(self._align_data().items())
+        self.dataset = list(self._align_data().values())
 
     def _align_data(self):
         """Aligns data in xsum, factuality, and faithfulness datasets:
@@ -21,19 +21,16 @@ class XsumDataset(Dataset):
                     'summary': system summary,
                     'labels': Dict(worker_id: is_factual)
                 },
-                'faithfulness_data':[
-                    Dict(system name: {
+                'faithfulness_data': Dict(system name: {
                         'summary': system summary,
-                        'hallucinations': Dict(worker_id, {
+                        'labels': Dict(worker_id, {
                             'hallucination_type': hallucination type,
                             'hallucinated_span': hallucinated span,
                             'hallucinated_span_start': hallucinated span start,
                             'hallucinated_span_end': hallucinated span end
                         })
-                    }
-                ]
-            }
-        )
+                    })
+                })
 
         """
 
@@ -51,11 +48,12 @@ class XsumDataset(Dataset):
             for data in self.factuality_data:
                 if str(data["bbcid"]) not in dataset:
                     dataset[str(data["bbcid"])] = {
-                        "document": None,
-                        "true_summary": None,
+                        "document": "",
+                        "true_summary": "",
                         "factuality_data": {},
                         "faithfulness_data": {},
                     }
+
                 if data["system"] in dataset[str(data["bbcid"])]["factuality_data"]:
                     dataset[str(data["bbcid"])]["factuality_data"][data["system"]][
                         "labels"
@@ -63,23 +61,25 @@ class XsumDataset(Dataset):
                 else:
                     dataset[str(data["bbcid"])]["factuality_data"][data["system"]] = {
                             "summary": data["summary"],
-                            "labels": {data["worker_id"]: data["is_factual"]},
+                            "labels": {data["worker_id"]: data["is_factual"]}
                         }
                     
         if self.faithfulness_data is not None:
             for data in self.faithfulness_data:
                 if str(data["bbcid"]) not in dataset:
                     dataset[str(data["bbcid"])] = {
-                        "document": None,
-                        "true_summary": None,
+                        "document": "",
+                        "true_summary": "",
                         "factuality_data": {},
                         "faithfulness_data": {},
                     }
+
                 if data["system"] in dataset[str(data["bbcid"])]["faithfulness_data"]:
-                    dataset[str(data["bbcid"])]["faithfulness_data"][data["system"]][str(data["worker_id"])] = {
+                    # spans of hallucinations seem off by one character --> one-indexing vs zero-indexing?
+                    dataset[str(data["bbcid"])]["faithfulness_data"][data["system"]]["labels"][str(data["worker_id"])] = {
                         "hallucination_type": data["hallucination_type"],
                         "hallucinated_span": data["summary"][
-                            data["hallucinated_span_start"] : data[
+                            data["hallucinated_span_start"]-1 : data[
                                 "hallucinated_span_end"
                             ]
                         ],
@@ -88,15 +88,18 @@ class XsumDataset(Dataset):
                     }
                 else:
                     dataset[str(data["bbcid"])]["faithfulness_data"][data["system"]] = {
-                        str(data["worker_id"]): {
-                            "hallucination_type": data["hallucination_type"],
-                            "hallucinated_span": data["summary"][
-                                data["hallucinated_span_start"] : data[
-                                    "hallucinated_span_end"
-                                ]
-                            ],
-                            "hallucinated_span_start": data["hallucinated_span_start"],
-                            "hallucinated_span_end": data["hallucinated_span_end"],
+                        "summary": data["summary"],
+                        'labels': {
+                            str(data["worker_id"]): {
+                                "hallucination_type": data["hallucination_type"],
+                                "hallucinated_span": data["summary"][
+                                    data["hallucinated_span_start"]-1 : data[
+                                        "hallucinated_span_end"
+                                    ]
+                                ],
+                                "hallucinated_span_start": data["hallucinated_span_start"],
+                                "hallucinated_span_end": data["hallucinated_span_end"],
+                            }
                         }
                     }
         return dataset
@@ -110,15 +113,15 @@ class XsumDataset(Dataset):
 
 data_xsum = load_dataset("xsum")  # document (string), summary (string), id (string)
 data_xsum_factuality = load_dataset(
-    "xsum_factuality"
+    "xsum_factuality", "xsum_factuality"
 )  # bbcid (int), system (string), summary (string), is_factual (class label), worker_id (string)
 data_xsum_faithfulness = load_dataset(
     "xsum_factuality", "xsum_faithfulness"
 )  # bbcid,system,summary,hallucination_type,hallucinated_span_start,hallucinated_span_end,worker_id
 
-train_data = XsumDataset(
-    data_xsum["train"], data_xsum_factuality["train"], data_xsum_faithfulness["train"]
-)
+
+# Dataloaderes for xsum data (without factuality or faithfulness)
+train_data = XsumDataset(data_xsum["train"])
 val_data = XsumDataset(data_xsum["validation"])
 test_data = XsumDataset(data_xsum["test"])
 
@@ -126,15 +129,19 @@ train_dl = DataLoader(train_data, batch_size=4, shuffle=False)
 val_dl = DataLoader(val_data, batch_size=4, shuffle=False)
 test_dl = DataLoader(test_data, batch_size=4, shuffle=False)
 
-for item in train_dl:
-    print(item)
+for item in val_data:
+    pprint.PrettyPrinter(indent=4).pprint(item)
+    break
+
+for item in test_data:
+    pprint.PrettyPrinter(indent=4).pprint(item)
     break
 
 
-for item in val_dl:
-    print(item)
-    break
+# For compiled data (for data vis only?), we can just enumerate over the dataset-- no dataloader. 
+compiled_data = XsumDataset(data_xsum["test"], data_xsum_factuality["train"], data_xsum_faithfulness["train"])
 
-# TODO
-# issues I see:
-# spans of hallucinations seem off by one character --> one-indexing vs zero-indexing?
+for _, item in enumerate(compiled_data):
+    pprint.PrettyPrinter(indent=4).pprint(item)
+    if len(item["factuality_data"]) > 0:
+        break
