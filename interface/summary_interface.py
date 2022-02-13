c3d7c8c94fa0c22b404116b64cd337d74ab5883f
cephcyn <cephcyn@gmail.com>
2022-02-13 03:40:33 -0500

    add super naive summary-source text matching search, no rendering yet

--- interface/summary_interface.py
+++ interface/summary_interface.py
@@ -10,6 +10,7 @@ from utils.faithfulness_annotations import (
 )
 import annotated_text
 import pandas as pd
+import regex as re
 
 
 @st.experimental_memo
@@ -122,6 +123,70 @@ _bert score:_ **TODO**
 
     st.subheader("Source Document")
     st.write(selected_data["document"])
+
+    st.write("---")
+
+    st.subheader("Summary-Source Links")
+
+    selected_summsrc = str(
+        st.selectbox(
+            "Select summary source", options=["Gold"] + sorted(df_factuality.index)
+        )
+    )
+    # Save the summary and source texts
+    selected_summsrc_summ = selected_faithfulness[
+        selected_faithfulness.system == selected_summsrc
+    ].iloc[0]["summary"]
+    selected_summsrc_src = selected_data["document"]
+
+    # Do a search for spans (length>=2) in summary that are contained within the document
+    linked_spans = {}
+    # TODO can definitely be more efficient, this is extremely lazy
+    searching_spans = [(0, len(selected_summsrc_summ))]
+    # TODO use actual proper substring/token recognition
+    last_end = len(selected_summsrc_summ)
+    while last_end > 0:
+        new_spans = [
+            (i.start() + 1, i.end() - 1)
+            for i in re.finditer(
+                r"[ \".,'].*[ \".,']", selected_summsrc_summ[:last_end], overlapped=True
+            )
+        ]
+        searching_spans += new_spans
+        searching_spans = [e for e in searching_spans if e[1] - e[0] >= 3]
+        if len(new_spans) == 0:
+            break
+        last_end = new_spans[-1][0] + 1
+    searching_spans = sorted(set(searching_spans), key=lambda x: x[0])
+    # scan through every substring/token span for matches
+    for (i, j) in searching_spans:
+        match_list = re.finditer(
+            selected_summsrc_summ[i:j], selected_summsrc_src, re.IGNORECASE
+        )
+        match_list = [(m.start(0), m.end(0)) for m in match_list]
+        if len(match_list) > 0:
+            linked_spans[(i, j)] = match_list
+    # pick what span matches to prioritize highlighting
+    # TODO: currently prioritizing span length solely, not sure how to handle overlapping matches
+    searching_matches = sorted(linked_spans, key=lambda x: x[1] - x[0], reverse=True)
+    # filter out overlapping highlights
+    highlights = []
+    for s in searching_matches:
+        if all([(s[1] <= h[0] or s[0] >= h[1]) for h in highlights]):
+            highlights += [s]
+    # TODO: render the search findings
+    st.write(
+        [
+            (
+                selected_summsrc_summ[k[0] : k[1]],
+                len(linked_spans[k]),
+                str(k),
+                str(linked_spans[k]),
+            )
+            for k in highlights
+        ]
+    )
+    st.write(selected_summsrc_summ)
 
 
 if __name__ == "__main__":
