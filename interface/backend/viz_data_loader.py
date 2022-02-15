2e72e6072d5241e0ebb65b0d195cabe90c3d601a
Anton Abilov <anton.abilov@gmail.com>
2022-02-14 16:36:51 -0800

    Integrate visualization data loader with XsumDataset class #35

--- interface/backend/viz_data_loader.py
+++ interface/backend/viz_data_loader.py
@@ -1,95 +1,44 @@
+import streamlit as st
 from datasets import load_dataset
+from sumtool.xsum_dataset import XsumDataset
 
 
-def collapse_factuality_annotations(annotations):
-    """
-    Collapse factuality annotations for each system across the workers
-    and calculate their mean factuality score
-
-    Args:
-        annotations (list[str]): annotions for a given article
-
-    Returns:
-        list of reduced annotions - one per system (list[dict])
-    """
-
-    collapsed = []
-    for system in ["BERTS2S", "TranS2S", "TConvS2S", "PtGen"]:
-        summary = None
-        sum_factual = 0
-        total = 0
-        for annotation in annotations:
-            if annotation["system"] == system:
-                summary = annotation["summary"]
-                total += 1
-                sum_factual += annotation["is_factual"]
-
-        if summary is not None:
-            collapsed.append(
-                {
-                    "system": system,
-                    "generated_summary": summary,
-                    "mean_worker_factuality_score": sum_factual / total,
-                }
-            )
-
-    return collapsed
-
-
-def load_annotated_data_by_id():
-    """
-    Load xsum dataset with factuality & faithfulness annotations
-
-    Returns:
-        Dict(article_id, {
-            id: str,
-            ground_truth_summary: str,
-            document: str,
-            factuality: [{
-                system: [BERTS2S, TRANS2S, TConvS2S, PTGen],
-                generated_summary: str,
-                mean_worker_factuality_score: float
-            }]
-            faithfulness: [{
-                ...Todo...
-            }]
-        })
-    """
-    # TODO: use solution from https://github.com/cs6741/summary-analysis/issues/2 to align data
-    data_xsum = load_dataset("xsum")
-    data_factuality = load_dataset("xsum_factuality")["train"]
-    data_faithfulness = load_dataset("xsum_factuality", "xsum_faithfulness")["train"]
-
-    annotated_data_by_id = {}
-    for annotation in data_factuality:
-        bbcid = str(annotation["bbcid"])
-        if bbcid in annotated_data_by_id:
-            annotated_data_by_id[bbcid]["factuality"].append(annotation)
-        else:
-            annotated_data_by_id[bbcid] = {
-                "id": bbcid,
-                "factuality": [annotation],
-                "faithfulness": [],
-            }
-
-    for annotation in data_faithfulness:
-        bbcid = str(annotation["bbcid"])
-        if bbcid in annotated_data_by_id:
-            annotated_data_by_id[bbcid]["faithfulness"].append(annotation)
-        else:
-            annotated_data_by_id[bbcid] = {
-                "id": bbcid,
-                "faithfulness": [annotation],
-                "factuality": [],
-            }
-
-    for key in annotated_data_by_id.keys():
-        annotated_data_by_id[key]["factuality"] = collapse_factuality_annotations(
-            annotated_data_by_id[key]["factuality"]
+def prepare_data_for_view(data):
+    factuality_data = {}
+    for system in data["factuality_data"].keys():
+        labels = data["factuality_data"][system]["labels"]
+        factuality_data[system] = data["factuality_data"][system]
+        factuality_data[system]["mean_worker_factuality_score"] = (
+            sum(labels.values()) / len(labels)
         )
 
-    for row in data_xsum["test"]:
-        if row["id"] in annotated_data_by_id:
-            annotated_data_by_id[row["id"]]["ground_truth_summary"] = row["summary"]
-            annotated_data_by_id[row["id"]]["document"] = row["document"]
-    return annotated_data_by_id
+    flat_faithfulness_data = []
+    for val in data["faithfulness_data"].values():
+        for labels in val["labels"].values():
+            flattened = {
+                "system": val["system"],
+                "summary": val["summary"],
+            }
+            for key, value in labels.items():
+                flattened[key] = value
+            flat_faithfulness_data.append(flattened)
+
+    return {
+        "document": data["document"],
+        "factuality_data": factuality_data,
+        "faithfulness_data": flat_faithfulness_data
+    }
+
+@st.experimental_memo
+def load_annotated_data_by_id():
+    dataset = XsumDataset(
+        load_dataset("xsum")["test"],
+        factuality_data=load_dataset("xsum_factuality")["train"],
+        faithfulness_data=load_dataset("xsum_factuality", "xsum_faithfulness")["train"]
+    )
+    view_dataset = {
+        k: prepare_data_for_view(v)
+        for k, v in dataset.data_by_id.items()
+        if len(v["faithfulness_data"]) > 0
+    }
+    return view_dataset
