94ed6e8d385f9623252ef236c4d2b47c1b92ddd9
Anton Abilov <anton.abilov@gmail.com>
2022-02-22 11:51:07 -0500

    WIP Calculate & show entropy for generated summaries #52

--- interface/model_interface.py
+++ interface/model_interface.py
@@ -1,10 +1,54 @@
 import streamlit as st
+from sumtool.predict_xsum_summary import predict_summary, load_summarization_model_and_tokenizer
+from backend.viz_data_loader import load_annotated_data_by_id
+import pandas as pd
+
+annotated_data_by_id = load_annotated_data_by_id()
 
 
 def render_model_interface():
-    st.header("Custom Transformer Model")
+    st.header("Generate a Summary")
 
-    # Your code goes here
+    model, tokenizer = load_summarization_model_and_tokenizer()
+
+    selected_id = str(
+        st.selectbox("Select entry by bbcid", options=annotated_data_by_id.keys())
+    )
+    xsum_example = annotated_data_by_id[selected_id]
+
+    st.subheader("Document")
+    st.write(xsum_example["document"])
+
+    num_beams = st.number_input("Number of beams", value=2)
+    
+    with st.spinner("Generating summary..."):
+        summary, summary_analysis, score = predict_summary(
+            model,
+            tokenizer,
+            xsum_example["document"],
+            num_beams=num_beams,
+            analyze_prediction=True
+        )
+        st.write(f"Summary score: {score}")
+        st.dataframe(pd.DataFrame(
+            summary_analysis,
+            columns=[
+                "Selected Token (id)" ,
+                "Prob",
+                "Beam idx",
+                "Local Entropy",
+                "In Source Doc?",
+                "B1 Top 1",
+                "B1 Top 1 Prob",
+                "B1 Top 2",
+                "B1 Top 2 Prob",
+                "B1 Top 3",
+                "B1 Top 3 Prob",
+            ]
+        ))
+
+    st.subheader("Generated Summary")
+    st.write(summary)
 
 
 if __name__ == "__main__":
--- sumtool/predict_xsum_summary.py
+++ sumtool/predict_xsum_summary.py
@@ -2,18 +2,18 @@ import argparse
 import torch
 import datasets
 from typing import Tuple
-from xsum_dataset import XsumDataset
-
-from transformers import BartTokenizer, BartForConditionalGeneration
+from sumtool.xsum_dataset import XsumDataset
+from transformers import BartTokenizer, BartForConditionalGeneration, BeamSearchScorer, LogitsProcessor, LogitsProcessorList, MinLengthLogitsProcessor, StoppingCriteriaList
+from scipy.stats import entropy
 
 
 def load_summarization_model_and_tokenizer(
-    device: torch.cuda.Device,
+    device: torch.cuda.Device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ) -> Tuple[BartForConditionalGeneration, BartTokenizer]:
     """
     Load summary generation model and move to GPU, if possible.
 
-    Args:
+    Optional args:
         device: cpu or gpu
 
     Returns:
@@ -26,10 +26,22 @@ def load_summarization_model_and_tokenizer(
     return model, tokenizer
 
 
+def calculate_entropy(seq_scores):
+    return torch.vstack([
+        torch.tensor(entropy(torch.nn.functional.softmax(
+            scores
+        ), axis=1))
+        for scores in seq_scores
+    ])
+
+
 def predict_summary(
     model: BartForConditionalGeneration,
     tokenizer: BartTokenizer,
     text_to_summarize: str,
+    analyze_prediction: bool = False,
+    num_beams: int = 4,
+    device: torch.cuda.Device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ) -> str:
     """
     Given a trained summary generation model and appropriate tokenizer,
@@ -43,6 +55,10 @@ def predict_summary(
         tokenizer: tokenizer corresponding to model
         text_to_summarize: document to summarize
 
+    Optional args:
+        analyze_prediction: whether prediction should be analyzed
+        device: cpu or gpu
+
     Returns:
         decoded_sentence
     """
@@ -54,21 +70,67 @@ def predict_summary(
     )
     input_token_ids = inputs.input_ids.to(device)
 
-    summary_ids = model.generate(
+    model_output = model.generate(
         input_token_ids,
-        num_beams=4,
+        num_beams=num_beams,
         max_length=150,
         early_stopping=True,
+        return_dict_in_generate=True,
+        output_scores=True,
+        num_return_sequences=1
     )
 
     predicted_summary = [
         tokenizer.decode(
             id, skip_special_tokens=True, clean_up_tokenization_spaces=False
         )
-        for id in summary_ids
+        for id in model_output.sequences
     ]
 
-    return predicted_summary[0]
+    if not analyze_prediction:
+        return predicted_summary[0]
+
+    else:
+
+        seq_idx = 0
+        prediction_analysis = []
+        input_set = input_token_ids.view(-1).tolist()
+
+        if num_beams == 1:
+            for idx, score in enumerate(model_output.scores):
+                probs = torch.nn.functional.softmax(
+                    model_output.scores[idx],
+                    dim=1
+                ).view(-1)
+                output_token_id = model_output.sequences[seq_idx][idx + 1]
+                prediction_analysis.append((
+                    output_token_id.item(),
+                    tokenizer.decode(output_token_id),
+                    entropy(probs),
+                    output_token_id in input_set
+                ))
+        else:
+            for idx, output_token_id in enumerate(model_output.sequences[seq_idx][1:]):
+                beam_idx = model_output.beam_indices[seq_idx][idx]
+                beam_probs = torch.exp(model_output.scores[idx][beam_idx])
+                beam_token_id = output_token_id
+
+                alternatives = []
+                top_probs = torch.topk(beam_probs, k=3)
+                for i, v in zip(top_probs.indices, top_probs.values):
+                    alternatives.append(f"{tokenizer.decode(i)} ({i.item()})")
+                    alternatives.append(v.item())
+
+                prediction_analysis.append((
+                    f"{tokenizer.decode(output_token_id)} ({beam_token_id.item()})",
+                    beam_probs[output_token_id].item(),
+                    beam_idx.item(),
+                    entropy(beam_probs).round(3),
+                    output_token_id in input_set,
+                    *alternatives
+                ))
+
+        return predicted_summary[0], prediction_analysis, model_output.sequences_scores.item()
 
 
 if __name__ == "__main__":
