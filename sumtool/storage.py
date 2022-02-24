5ea87d5b932c79f5ae09776e6f761b24801d9709
Anton Abilov <anton.abilov@gmail.com>
2022-02-24 12:48:08 -0500

    Added getters for datasets, models and summaries

--- sumtool/storage.py
+++ sumtool/storage.py
@@ -3,16 +3,18 @@ import hashlib
 import os
 from typing import Dict, Optional, Any
 
+STORAGE_DIR = "./data"
 
-def storage_dir(dataset):
-    return f"./data/{dataset}"
+
+def dataset_dir(dataset):
+    return f"{STORAGE_DIR}/{dataset}"
 
 
 def store_model_config(dataset: str, model: str, model_config: dict):
     model_config_json = json.dumps(model_config, sort_keys=True, indent=2)
     model_config_hash = hashlib.md5(model_config_json.encode()).hexdigest()
 
-    dir = f"{storage_dir(dataset)}/models"
+    dir = f"{dataset_dir(dataset)}/models"
     path = f"{dir}/{model.replace('/', '-')}-{model_config_hash}.json"
 
     if not os.path.exists(path):
@@ -60,7 +62,7 @@ def store_model_summaries(
     """
 
     model_config_hash = store_model_config(dataset, model, model_config)
-    dir = storage_dir(dataset)
+    dir = dataset_dir(dataset)
     path = f"{dir}/{model.replace('/', '-')}-summaries.json"
 
     if not os.path.exists(path):
@@ -113,7 +115,7 @@ def store_summary_metrics(
 
     """
 
-    dir = storage_dir(dataset)
+    dir = dataset_dir(dataset)
     path = f"{dir}/{model.replace('/', '-')}-metrics.json"
 
     if not os.path.exists(path):
@@ -138,3 +140,20 @@ def store_summary_answers(
     # model_config_hash: str
 ):
     raise Exception("Not implemented!")
+
+
+def get_datasets():
+    return os.listdir(STORAGE_DIR)
+
+
+def get_models(dataset: str):
+    return [
+        x.replace("-summaries.json", "")
+        for x in os.listdir(dataset_dir(dataset))
+        if "-summaries.json" in x
+    ]
+
+
+def get_summaries(dataset: str, model: str):
+    with open(f"{dataset_dir(dataset)}/{model}-summaries.json", "r") as f:
+        return json.load(f)
