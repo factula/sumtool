440a84bd3d74192e35e88943d6dd998f1541f15f
vincehartman38 <3585333+vincehartman38@users.noreply.github.com>
2022-03-02 22:58:13 -0500

    make model interface layer fully cached

--- interface/model_interface.py
+++ interface/model_interface.py
@@ -3,8 +3,11 @@ from sumtool import generate_xsum_summary, storage
 from backend.viz_data_loader import load_annotated_data_by_id
 import pandas as pd
 
+cache_summaries = storage.get_summaries("xsum", "facebook-bart-large-xsum")
+cache_summary_values = [*cache_summaries.values()][0]
+cache_keys = [x for x in [*cache_summary_values.keys()]]
 annotated_data_by_id = load_annotated_data_by_id()
-
+filtered_annotated_data_by_id = {k: annotated_data_by_id[k] for k in cache_keys if k in annotated_data_by_id}
 
 @st.experimental_singleton
 def cache_load_summarization_model_and_tokenizer():
@@ -13,13 +16,13 @@ def cache_load_summarization_model_and_tokenizer():
 
 def render_model_interface():
 
-    st.header("Generate a Summary")
+    st.header("Generate a Summary with BART XSUM Model")
 
     # Select/Input source document
     selected_id = str(
-        st.selectbox("Select entry by bbcid", options=annotated_data_by_id.keys())
+        st.selectbox("Select entry by bbcid", options=filtered_annotated_data_by_id.keys())
     )
-    selected_data = annotated_data_by_id[selected_id]
+    selected_data = filtered_annotated_data_by_id[selected_id]
     source = selected_data["document"]
     st.subheader("Source Document")
     st.write(source)
@@ -29,31 +32,12 @@ def render_model_interface():
     g_summary = (
         selected_faithfulness[selected_faithfulness.system == "Gold"].iloc[0].summary
     )
-
     st.subheader("Ground Truth Summary")
     st.write(g_summary)
 
-    cache_summaries = storage.get_summaries("xsum", "facebook-bart-large-xsum")
-    cache_summary_values = [*cache_summaries.values()][0]
-    if selected_id in cache_summary_values:
-        predicted_summary = cache_summary_values[selected_id]["summary"][0]
-    else:
-        with st.spinner("Loading pre-trained model"):
-            model, tokenizer = cache_load_summarization_model_and_tokenizer()
-
-        with st.spinner("Generating summary..."):
-            predicted_summary = generate_xsum_summary.generate_summaries(
-                model, tokenizer, source
-            )[0]
-        storage.store_model_summaries(
-            "xsum",
-            model.config.name_or_path,
-            model.config.to_dict(),
-            {selected_id: predicted_summary},
-        )
-
     # Output summarization
-    st.subheader("Predicted Summary with BART XSUM Model")
+    predicted_summary = cache_summary_values[selected_id]["summary"][0]
+    st.subheader("Predicted Summary")
     st.write(predicted_summary)
 
 
--- sumtool/generate_all_xsum_summaries.py
+++ sumtool/generate_all_xsum_summaries.py
@@ -1,74 +1,11 @@
 import argparse
-import torch
 import datasets
-from typing import List, Tuple
+from sumtool.generate_xsum_summary import (
+    load_summarization_model_and_tokenizer,
+    generate_summaries,
+)
 from sumtool.xsum_dataset import XsumDataset
 from sumtool.storage import store_model_summaries
-from transformers import BartTokenizer, BartForConditionalGeneration
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-def load_summarization_model_and_tokenizer() -> Tuple[
-    BartForConditionalGeneration, BartTokenizer
-]:
-    """
-    Load summary generation model and move to GPU, if possible.
-
-    Returns:
-        (model, tokenizer)
-    """
-    tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-xsum")
-    model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-xsum")
-    model.to(device)
-
-    return model, tokenizer
-
-
-def generate_summaries(
-    model: BartForConditionalGeneration,
-    tokenizer: BartTokenizer,
-    docs_to_summarize: List[str],
-) -> str:
-    """
-    Given a trained summary generation model and appropriate tokenizer,
-
-    1. Tokenize text (and move to device, if possible)
-    2. Run inference on model to generate output vocabulary tokens for summary
-    3. Decode tokens to a sentence using the tokenizer
-
-    Args:
-        model: model to run inference on
-        tokenizer: tokenizer corresponding to model
-        docs_to_summarize: documents to summarize
-
-    Returns:
-        decoded_sentence
-    """
-    inputs = tokenizer(
-        docs_to_summarize,
-        max_length=1024,
-        truncation=True,
-        return_tensors="pt",
-        padding=True,
-    )
-    input_token_ids = inputs.input_ids.to(device)
-
-    summary_ids = model.generate(
-        input_token_ids,
-        num_beams=4,
-        max_length=150,
-        early_stopping=True,
-    )
-
-    generated_summaries = [
-        tokenizer.decode(
-            id, skip_special_tokens=True, clean_up_tokenization_spaces=False
-        )
-        for id in summary_ids
-    ]
-
-    return generated_summaries
 
 
 if __name__ == "__main__":
